9e77836b170d1453ceefa0fe178a2f09413e1160
nakaji <nakaji126@gmail.com>
2014-10-30 02:58:50 +0900

    アプリケーション用のDatabaseContextを追加

---
+++ NGDS.Web/DataContexts/DrinksDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using NGDS.Web.Models;
+
+namespace NGDS.Web.DataContexts
+{
+    public class DrinksDb : DbContext
+    {
+        public DrinksDb() : base("DefaultConnection") { }
+
+        public DbSet<Drink> Drinks { get; set; }
+    }
+}
