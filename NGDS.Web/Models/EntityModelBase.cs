ac6f93a5af7034db945229c821014f5b2b5acf46
nakaji <nakaji126@gmail.com>
2014-11-07 02:05:49 +0900

    表示用の名称を設定

--- NGDS.Web/Models/EntityModelBase.cs
+++ NGDS.Web/Models/EntityModelBase.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
 
 namespace NGDS.Web.Models
 {
     public class EntityModelBase
     {
+        [DisplayName("登録日")]
         public DateTime CreateDateTime { get; set; }
+
+        [DisplayName("更新日")]
         public DateTime UpdateDateTime { get; set; }
     }
 }
